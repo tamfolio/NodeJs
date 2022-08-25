3ea215dda70692e3723256bc833df6ae4b13c715
unknown <tamiloreolaniyi@gmail.com>
2022-08-25 23:28:25 +0100

    mongodb commands

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 
 
@@ -22,6 +23,36 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+
+
 
 
 
